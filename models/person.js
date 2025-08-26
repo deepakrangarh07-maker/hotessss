3df6465136f3a29fcacc89620004c8600d59f504
Deepak Rangarh <deepakrangarh07@gmail.com>
2025-08-26 19:19:22 +0530

    this 26 aug

--- models/person.js
+++ models/person.js
@@ -1,6 +1,7 @@
 const req = require("express/lib/request");
 const { type } = require("express/lib/response");
 const mongoose = require("mongoose");
+const bcrypt = require ("bcrypt");
 
 const personSchema = new mongoose.Schema({
     name:{
@@ -29,8 +30,54 @@ const personSchema = new mongoose.Schema({
     salary:{
         type:Number,
         required: true
+    },
+    username:{
+        type:String,
+        required:true
+    },
+    password:{
+        required:true,
+        type:String
     }
 });
+personSchema.pre("save" , async function (next){
+    const person = this;
+    if(!person.isModified("password"))return next();
+    //Hash the password only if it has been modified (or is new);
+    try{
+        // hash password generation 
+        const salt = await bcrypt.genSalt(10);
+
+        //hash password
+        const hashedpassword = await bcrypt.hash(person.password, salt);
+        person.password = hashedpassword;
+      next();  
+    }
+    catch(err){
+        return next(err);
+    }
+})
+personSchema.methods.comparePassword = async function(candidatePassword) {
+    try{
+        const isMatch = await bcrypt.compare(candidatePassword, this.password);
+            return isMatch;
+    }
+    catch(err){
+        threw(err);
+    }
+}
+// the compare funtion automatically extract the salt from the stored hashed password and use it to hash the entered password . it then compares the resulting hash with the stored hash . if they match  it indicated that the entered that the password is correct.
+
+
+// prince ---> nndnnsdskfas
+// login ----> deepakpassword
+
+// nndnnsdskfas --> extract salt 
+// salt + deepakpassword ----> hash ---> lkdjfasdsndf 
+
+// campare the password nndnnsdskfas || lkdjfasdsndf
+// if incorrect then login failed
+
 
 const Person = mongoose.model("Person", personSchema);
 module.exports = Person;    
--- server.js
+++ server.js
@@ -3,13 +3,26 @@ const app = express();
 const db = require("./db");
 const bodyParser =  require("body-parser");
 require("dotenv").config();
+const passport = require("./auth");
 
 // 
 const PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.json());  
 
-app.get("/", (req, res) =>{
+
+
+// middleware function 
+const logRequest = (req, res, next) =>{
+    console.log(`[${new Date().toLocaleString()}] Request Made to : ${req.originalUrl}`);
+    next(); //Move on the next phase : 
+;}
+app.use(logRequest); 
+// Aunthentication ans authorization 
+// Login and Signup 
+app.use(passport.initialize());
+const authPassword = passport.authenticate("local",{session: false})
+app.get("/",(req, res) =>{
     res.send("hello world is printing")
 }); 
 
@@ -18,7 +31,7 @@ app.get("/", (req, res) =>{
 
 
 const personRoutes = require("./Router/personRoutes");
-app.use("/person" ,personRoutes);
+app.use("/person" ,authPassword,personRoutes);
 
 const feedbackRoutes = require("./Router/feedback");
 app.use("/feedback", feedbackRoutes);
@@ -32,3 +45,5 @@ app.listen(PORT, ()=>{
     console.log("server is listening")
 });
  
+
+
